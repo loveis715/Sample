e8e531bcde17115a33c68f5be1c8504b28629228
Hanyu Zhang <silverfox715@sina.com>
2016-04-02 15:27:16 +0800

    Add some simple audit log Then we can track user operations on orders.
    Build successfully
    
    Next step: Add order processing logic

--- core/server/src/main/java/com/ambergarden/orderprocessor/service/OrderService.java
+++ core/server/src/main/java/com/ambergarden/orderprocessor/service/OrderService.java
@@ -3,9 +3,12 @@ package com.ambergarden.orderprocessor.service;
 import java.util.Date;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ambergarden.orderprocessor.HomeController;
 import com.ambergarden.orderprocessor.converter.OrderConverter;
 import com.ambergarden.orderprocessor.exception.BadOrderRequestException;
 import com.ambergarden.orderprocessor.exception.OrderNotFoundException;
@@ -20,6 +23,8 @@ import com.ambergarden.orderprocessor.schema.beans.order.StepStatus;
  */
 @Service
 public class OrderService {
+   private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
    @Autowired
    private OrderRepository orderRepository;
 
@@ -27,10 +32,14 @@ public class OrderService {
    private OrderConverter orderConverter;
 
    public List<Order> findAll() {
+      logger.info("OrderService - findAll() invoked");
+
       return orderConverter.convertListFrom(orderRepository.findAll());
    }
 
    public Order create(Order order) {
+      logger.info("OrderService - create() invoked");
+
       validOrderForCreation(order);
 
       // TODO: Check whether user has specified id.
@@ -42,10 +51,15 @@ public class OrderService {
          = orderConverter.convertTo(order);
       orderMO.setId(-1); // PostgreSQL uses -1 for creation.
       orderMO = orderRepository.save(orderMO);
+
+      logger.info("OrderService - create() invoked successful: order id: " + order.getId());
+
       return orderConverter.convertFrom(orderMO);
    }
 
    public Order findById(int id) {
+      logger.info("OrderService - findById() invoked");
+
       com.ambergarden.orderprocessor.orm.entity.order.Order result = orderRepository.findOne(id);
       if (result != null) {
          return orderConverter.convertFrom(result);
